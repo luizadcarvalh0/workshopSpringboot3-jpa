caaa62f784cf9544e14be8585731ebe667159fd3
Maria Luiza <luizadamiao16@gmail.com>
2024-05-12 00:57:10 -0300

    Exception handling - delete

---
+++ src/main/java/com/exerciciospring/course/services/exceptions/DataBaseException.java
@@ -0,0 +1,10 @@
+package com.exerciciospring.course.services.exceptions;
+
+public class DataBaseException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+}
